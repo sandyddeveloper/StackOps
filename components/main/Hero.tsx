c1af52963ca5de41c33507850a3127a9cf86422a
sandyddeveloper <sandyddeveloper@gmail.com>
2025-03-06 19:47:37 +0530

    font again changed

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import HeroContent from '../sub/HeroContent'
-import Image from 'next/image'
+"use client"; // Ensure this is a client component for Framer Motion
+
+import React from "react";
+import HeroContent from "../sub/HeroContent";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div className='relative flex flex-col h-full w-full'>
-        <Image 
-          src="/top5.gif" 
+    <div className="relative flex flex-col h-full w-full overflow-hidden">
+      <motion.div
+        initial={{ y: -540, opacity: 0 }} 
+        animate={{ y: 0, opacity: 1 }} 
+        transition={{ duration: 1.5, ease: "easeOut" }} 
+        className="absolute top-[-540px] left-0 z-[1] w-full h-full"
+      >
+        <Image
+          src="/top5.gif"
           width={1200}
           height={750}
-          alt="Animated GIF" 
-          className='rotate-180 absolute top-[-540px] left-0 z-[1] w-full h-full object-cover'
+          alt="Animated GIF"
+          className="rotate-180 w-full h-full object-cover"
         />
-        <HeroContent /> 
-    </div>
-  )
-}
+      </motion.div>
 
-export default Hero
+      <HeroContent />
+    </div>
+  );
+};
+
+export default Hero;
