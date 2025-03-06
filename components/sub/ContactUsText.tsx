8be7e6c65a146759c59b0c3e7c21d8c6bf97f83a
sandyddeveloper <sandyddeveloper@gmail.com>
2025-03-06 18:55:17 +0530

    dleted

--- components/sub/ContactUsText.tsx
+++ components/sub/ContactUsText.tsx
@@ -13,14 +13,14 @@ const TextUs = () => {
       >
         <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
         <h1 className="Welcome-text text-[13px]">
-        Let's Connect & Collaborate!
+        Let&apos;s Connect & Collaborate!
         </h1>
       </motion.div>
       <motion.div
         variants={slideInFromLeft(0.5)}
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
       >
-       Have a project in mind? Let's bring it to life!
+       Have a project in mind? Let&apos;s bring it to life!
       </motion.div>
       <motion.div
         variants={slideInFromRight(0.5)}
