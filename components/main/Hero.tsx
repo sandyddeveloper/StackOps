818b8f9d8f9f1a4831dd3c6e8e8c9271273cc1ee
sandyddeveloper <sandyddeveloper@gmail.com>
2025-03-06 18:19:47 +0530

    dleted

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import HeroContent from '../sub/HeroContent'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <div className='relative flex flex-col h-full w-full'>
-        <img 
+        <Image 
           src="/top5.gif" 
+          width={1200}
+          height={750}
           alt="Animated GIF" 
           className='rotate-180 absolute top-[-540px] left-0 z-[1] w-full h-full object-cover'
         />
