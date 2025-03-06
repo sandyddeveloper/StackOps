35b263ff4ec5431c4a0cfeef6f467032bfa46757
sandyddeveloper <sandyddeveloper@gmail.com>
2025-03-06 15:50:57 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        {/* <StarsCanvas /> */}
+        <StarsCanvas />
         <Navbar />
         {children}
         <ScrollToTop />
