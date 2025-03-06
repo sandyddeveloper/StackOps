14b292152f8e7274a5680517a5a16f203c48a383
sandyddeveloper <sandyddeveloper@gmail.com>
2025-03-06 19:32:59 +0530

    dleted

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 import React from 'react'
 import {motion} from 'framer-motion'
@@ -7,27 +8,27 @@ import { SparklesIcon } from '@heroicons/react/24/solid'
 const SkillText = () => {
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center'>
-      <motion.div
-        variants={slideInFromTop}
-        className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
-      >
-        <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-        <h1 className="Welcome-text text-[13px]">
+<motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
           Crafting Scalable Web Solutions
-        </h1>
-      </motion.div>
-      <motion.div
+          </h1>
+        </motion.div>
+        <motion.div
         variants={slideInFromLeft(0.5)}
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
-      >
-        Building Robust Apps with Modern Technologies
-      </motion.div>
-      <motion.div
+        >
+             Building Robust Apps with Modern Technologies
+        </motion.div>
+        <motion.div
         variants={slideInFromRight(0.5)}
         className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
-      >
-        Innovating with Full-Stack Development & Problem Solving
-      </motion.div>
+        >
+            Innovating with Full-Stack Development & Problem Solving
+        </motion.div>
     </div>
   )
 }
